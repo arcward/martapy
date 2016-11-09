5ba4f2c885a4dc09d1af24874424a2da2cc3caf1
Edward <me@edward.sh>
2016-11-08 23:28:25 -0500

    Added copyright notice, setup.py, updated __init__

---
+++ martapy/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name = "martapy",
+    version = "1.0",
+    author = "Edward Wells",
+    author_email = "send@edward.sh",
+    description = "A client for MARTA's API.",
+    license = "MIT",
+    keywords = "MARTA, rail, bus, API, Atlanta",
+    packages=['martapy']
+)
---
+++ martapy/tests/__init__.py
@@ -0,0 +1,3 @@
+__all__ = ['martapy']
+
+import martapy
